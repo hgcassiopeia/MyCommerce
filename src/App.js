cdc3864dbea3a71627f2c42711c95b0a924f036a
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2018-07-05 22:07:29 +0700

    Add Header and Footer Function Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './common/Header';
+import Footer from './common/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div>
+        <Header />
+        <Footer />
       </div>
     );
   }
